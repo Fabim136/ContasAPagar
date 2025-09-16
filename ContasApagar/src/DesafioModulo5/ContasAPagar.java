19841a97c49c6ce865c2b9f339535efb44f9f9a8
Fabiano Junior Alves da Silva <fabianojunior136@gmail.com>
2025-09-16 16:24:34 -0300

    classe fornecedor, contas a pagar e principal consturida. Necessario refatorar a classe principal

--- ContasApagar/src/DesafioModulo5/ContasAPagar.java
+++ ContasApagar/src/DesafioModulo5/ContasAPagar.java
@@ -1,5 +1,50 @@
 package DesafioModulo5;
 
 public class ContasAPagar {
+	private String descricao;
+	private int valor;
+	private String dataVencimento;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	public int getValor() {
+		return valor;		
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	public String getDataVencimento () {
+		return dataVencimento;
+	}
+	public void setDataVencimento(String dataVencimento) { 
+		this.dataVencimento = dataVencimento;
+	}
+	
+	Fornecedor fornecedor = new Fornecedor();
+	
 
+	
+	
+
+	ContasAPagar (String descricao, Fornecedor fornecedor, double valor, String dataVencimento) {
+		this.descricao = "teste";
+		//this.fornecedor;
+		this.valor = 32;
+		this.dataVencimento = "Venceu";
+		
+		
+	}
+	
+	public void pagar() {
+		
+	}
+	
 }
